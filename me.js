26f7d6fd5e47bc942dc6846ad1356d6686160b86
Pradeep email = pradeepsen99@gmail.com <pradeep@PradeepsComp.localdomain>
2017-12-01 23:24:05 -0600

    NUKE Pt.2

--- me.js
+++
@@ -1,8 +0,0 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 50) {
-        $('#scroller').addClass('stuck');
-    } else {
-        $('#scroller').removeClass('stuck');
-    }
-
-});
