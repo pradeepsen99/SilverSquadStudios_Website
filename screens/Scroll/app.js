64a7cc7d01251fab215daf919193a61ef93c1095
Tomas <tbindokas13@gmail.com>
2018-02-24 14:37:09 -0600

    Scroll website test

--- screens/Scroll/app.js
+++ screens/Scroll/app.js
@@ -1,4 +1,19 @@
-var bigOne = document.getElementById('bigOne');
-var dbRef = firebase.database().ref().child(text);
+// Initialize Firebase
+var config ={
+  apiKey: "AIzaSyCEmtGw8bbRb5FWp2RE4uFL46khLo1OGNk",
+  authDomain: "scroll-f9e43.firebaseapp.com",
+  databaseURL: "https://scroll-f9e43.firebaseio.com",
+  projectId: "scroll-f9e43",
+  storageBucket: "scroll-f9e43.appspot.com",
+  messagingSenderId: "422097259298"
+};
+var dataspace = 'Users/TestUser/1/Notes';
+firebase.initializeApp(config);
+window.alert("ok");
 
-dbRef.on('value',snap >= bigOne.innerText= snap.val());
+console.log(firebase);
+
+var db = firebase.database();
+var ref = db.ref('Users/TestUser/1/Notes');
+
+ref.on('value', snap=> firstmsg.innerText=snap.val());
